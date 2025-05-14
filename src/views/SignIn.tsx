bcedf0eb68502781d4370be96494637c385c5be8
rafarm04 <wafar0mo@hotmail.com>
2025-05-13 18:54:26 -0600

    Added all new endpoints

--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -11,7 +11,7 @@ function SignIn({ setShowSignUp }) {
 
   const handleSignIn = async () => {
     try {
-      const response = await fetch("/users/login", {
+      const response = await fetch("/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
