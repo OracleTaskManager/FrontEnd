f5b612031908f3038f542f90899a0b5d77e48b17
IsaiasFrausto <isaiasfrausto1107@gmail.com>
2025-05-28 10:24:56 -0600

    adding button to edit team, delete user and show users in team

--- src/views/DashboardDev.tsx
+++ src/views/DashboardDev.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Ticket from "../components/Ticket";
-import CircularProgress from "../components/CircularProgress";
+//import CircularProgress from "../components/CircularProgress";
 
 interface Ticketx {
   taskId: number;
