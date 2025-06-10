80ee398a4c940f669f49358f7a6cdb0daf9152ae
rafarm04 <wafar0mo@hotmail.com>
2025-06-10 10:04:20 -0600

    Display on ticket the user assigned to a task

--- src/components/Sprint.tsx
+++ src/components/Sprint.tsx
@@ -208,13 +208,13 @@ export default function Sprint({
               onClick={handleSave}
               className="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700"
             >
-              Guardar
+              Save
             </button>
             <button
               onClick={handleCancel}
               className="bg-gray-400 text-white px-3 py-1 rounded hover:bg-gray-500"
             >
-              Cancelar
+              Cancel
             </button>
           </div>
         </div>
@@ -238,7 +238,7 @@ export default function Sprint({
             onClick={handleDelete}
             className="mt-2 ml-2 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
           >
-            Eliminar
+            Delete
           </button>
         </div>
       )}
